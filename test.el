9e9df42cc1c950076e0bc309f6ccee6b2b072063
Marco Baringer <mb@bese.it>
2013-02-28 01:41:32 +0100

    Removed test script

--- test.el
+++
@@ -1,9 +0,0 @@
-(setf debug-on-error t
-      websocket-callback-debug-on-error t
-      )
-(add-to-list 'load-path "/Users/mb/m/.emacs/emacs-websocket/")
-(add-to-list 'load-path "/Users/mb/m/.emacs/js2-mode/")
-(add-to-list 'load-path "/Users/mb/m/.emacs/jss/")
-(defun dbg () (interactive) (setf deferred:debug-on-signal t))
-(require 'jss)
-
